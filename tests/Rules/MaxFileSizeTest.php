ed043a5e11f89793cc35381b21ccac1bad142555
S.M. Ataur Rahman <dev.ataur.rahman@gmail.com>
2020-10-11 20:50:33 +0600

    test for max size check in multiple files input

--- tests/Rules/MaxFileSizeTest.php
+++ tests/Rules/MaxFileSizeTest.php
@@ -20,6 +20,41 @@ final class MaxFileSizeTest extends AbstractFileRuleTestHelper implements RuleTe
         $this->performTest(['photos' => [['size' => 299], ['size' => 501]]], ['photos' => ['MaxFileSize' => 'Photos should be less than 500 bytes!']]);
     }
 
+    public function testInvalidInMultipleData()
+    {
+        $this->validator->file('photos')->maxSize(200);
+        $photos = ['photos' =>
+            [
+                [
+                    'name' => 'Screenshot_2020-09-05.png',
+                    'type' => 'image/png',
+                    'tmp_name' => '/tmp/php45TlTv',
+                    'size' => 199,
+                    'error' => 0,
+                ],
+                [
+                    'name' => 'Screenshot_2020-09-05.jpeg',
+                    'type' => 'image/jpeg',
+                    'tmp_name' => '/tmp/php45TlTv',
+                    'size' => 201,
+                    'error' => 0,
+                ],
+                [
+                    'name' => 'Screenshot_2020-09-05.gif',
+                    'type' => 'image/gif',
+                    'tmp_name' => '/tmp/php45TlTv',
+                    'size' => 200,
+                    'error' => 0,
+                ],
+            ],
+        ];
+
+        $this->performTest(
+            $photos,
+            ['photos' => ['MaxFileSize' => 'Photos should be less than 200 bytes!']]
+        );
+    }
+
     public function testValidData()
     {
         $this->validator->file('photos')->maxSize(500);
